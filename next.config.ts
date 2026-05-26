c2be7d7fd29a9849677580cf66dbfee24cd29544
Nobuhiko Hayashi <mosubaga@icloud.com>
2026-05-26 07:13:10 -0400

    fixed config

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+    output: 'export',
+    basePath: '/wmhelper',
+    trailingSlash: true,
+    images: {
+        unoptimized: true,
+    },
 };
 
 export default nextConfig;
